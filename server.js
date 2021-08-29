6963c9b08a5bf744d81b6e9f349942815041dfa0
Jiten Pandey <jitpandey@deloitte.com>
2021-08-30 00:49:46 +0530

    modified server.js

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.static("./dist/shorttly"));
+app.use(express.static("./dist/url-shortner-frontend"));
 
 app.get("/*", function (req, res) {
-  res.sendFile("index.html", { root: "dist/shorttly/" });
+  res.sendFile("index.html", { root: "dist/url-shortner-frontend/" });
 });
 app.listen(process.env.PORT || 8080);
